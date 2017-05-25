2bb009f58a47315d03caebe5a94f393e5c4f72cf
Michael Cittar <mcittar@gmail.com>
2017-05-25 16:41:04 -0700

    Test module

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-'use strict';
 
-/**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
- * @return {string}
- */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
+module.exports.Test = () => {
+  console.log("Working");
 };
