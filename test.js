e2198d8332563a94a79981fe8b60aef8924ff182
Michael Cittar <mcittar@gmail.com>
2017-05-30 17:00:20 -0700

    WORKING

--- test.js
+++ test.js
@@ -5,13 +5,21 @@ let jsonfile = require('jsonfile')
 
 const addScript = () => {
   try {
-    let _package = jsonfile.readFileSync('package.json');
+    let _package = jsonfile.readFileSync('../../package.json');
     if (!_package.scripts) {
       _package.scripts = {};
     }
 
+    console.log(_package);
+
     _package.scripts["boilerplate"] = "react-jarb";
-    jsonfile.writeFileSync('package.json', _package, {spaces: 2});
+
+    console.log(_package.scripts);
+
+    jsonfile.writeFileSync('../../package.json', _package, {spaces: 2});
+
+    console.log(jsonfile.readFileSync('../../package.json'));
+    console.log(_package);
   } catch (e) {
 
       throw e
